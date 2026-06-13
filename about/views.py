2309bc1fa0b7c72eb3ce2f4883ce5dcd9d84dc96
skb399 <stefanboakye@gmail.com>
2026-06-13 21:48:12 +0100

    add view functionality

--- about/views.py
+++ about/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def about_me(request):
+    return HttpResponse("Hello, this would be the about page")
